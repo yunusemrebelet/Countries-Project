788d9a948a67459006d3669cc4129384cee82074
yunuse <986873673>
2022-04-14 18:09:52 +0300

    develop branch has been opened and Header component added

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <div class="container">
+      <h1>Where in the world?</h1>
+      <!-- TODO: Add reactive class to toggle dark and light mode fas fa-moon far fa-moon -->
+      <button><i class="far fa-moon"></i> Dark Mode</button>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
